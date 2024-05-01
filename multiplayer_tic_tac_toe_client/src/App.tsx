97d1caced12ba1974a8ce2606b15dfee47e1a526
IMRANDIL <aliimranadil2@gmail.com>
2024-05-01 11:17:11 +0530

    added minimal css

--- multiplayer_tic_tac_toe_client/src/App.tsx
+++ multiplayer_tic_tac_toe_client/src/App.tsx
@@ -1,23 +1,18 @@
 
-import { useEffect } from 'react'
+
 import './App.css'
-import { io } from 'socket.io-client'
+import Box from './components/Box'
+
 
 function App() {
-  const connect = ()=>{
-    const socket = io("http://localhost:9000");
-    socket.on('connect', ()=>{
-      socket.emit('custom_event', {name: 'Ali', isPassionate: true})
-    })
-  }
 
-  useEffect(()=>{
-    connect()
-  },[])
 
   return (
-    <div>
-     <h1>Welcom to Tic_Tac_Toe</h1>
+    <div className='container'>
+     <h1 style={{
+      color: 'darkgray'
+     }}>Welcom to Tic_Tac_Toe</h1>
+     <Box/>
     </div>
   )
 }
