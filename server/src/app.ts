62b90a92b61d7ba068c43b3f7a38e27c762ff2bd
IMRANDIL <aliimranadil2@gmail.com>
2024-05-01 08:51:26 +0530

    fixed the typescript setup issue

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,42 @@
-import express, { Request, Response } from 'express';
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+import cors from "cors";
+import "reflect-metadata";
 
-const app = express();
+var indexRouter = require("./routes/index");
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, Express and TypeScript!');
+var app = express();
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
+
+export default app;
